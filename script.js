cab166133ccb9bbdd8ef640b06f58f0992924b35
terina-pro <110566538+terina-pro@users.noreply.github.com>
2022-08-19 14:55:53 +0300

    ДЗ 4
    Реализовала список покупок:
    - элементы добавляются в список по нажатию enter
    - присвоила элементу списка classList.toggle
    - добавила sendInput.value

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
-
 /* Пустые элементы не должны добавляться */
-
 /* Если кликнуть на элемент списка, он зачеркивается */
-
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
-
 /* Очищать input после добавления нового элемента в список */
+
+const sendInput = document.querySelector('#input');
+const itemsContainer = document.querySelector('.items');
+
+sendInput.addEventListener('keydown', function(event) {
+    const listItem = sendInput.value;
+    const newItem = document.createElement('div');
+
+    newItem.addEventListener('click', function() {
+        newItem.classList.toggle('done');
+    });
+
+    if (event.key == 'Enter') {
+        newItem.textContent = listItem;
+        if (listItem != '') {
+            itemsContainer.append(newItem);
+        }
+        sendInput.value = '';
+    }
+})
